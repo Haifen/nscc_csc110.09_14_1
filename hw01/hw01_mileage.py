cfd15116bccbbbbe1c41cce1204f57d538dc550f
Robin Kauffman <robink@creosotehill.org>
2014-09-29 00:14:40 -0700

    Update of first homework assignment
    Update of first homework assignment to have calculated values defined in
    a property, generated with a function, memoized, and serialized as
    human-parsable strings using Python 3's string interpolation support
    (str.format()).

--- hw01/hw01_mileage.py
+++ hw01/hw01_mileage.py
@@ -1,21 +1,50 @@
-# Program to calculate mile per gallon based on distance travelled and gallons used
-# mpg = miles/gallons
-# CSC 110
-# Fall 2014
+#!/bin/env python
 
-print ('This program will calculate mpg based on your input')
-print ()
-print ()
+import functools, textwrap
 
-# input section
-miles = float( input('How many miles did you travel? ') )
-gallons = float( input('How many gallons did you use? ') )
+# OK, I really really don't know what I'm doing w/r/t higher-order functions in
+# Python, all I know is this particular mix of function decorators and
+# function-passing gives me the result I want (behaves, memoizes, doesn't freak
+# out as a result of trying to serialize the parent class).
 
-# process
-mpg = miles/gallons
+class Mpg:
+	def trip_cost_memoizable():
+		@functools.lru_cache()
+		def trip_cost(v, c): return v * c
+		return trip_cost
+	def pergallon_memoizable():
+		@functools.lru_cache()
+		def pergallon(d, v): return d / v
+		return pergallon
 
-#output section
-print ('You travelled ',miles, 'miles')
-print ('You used ', gallons, 'gallons')
-print ('Your mpg = ', mpg)
+# Everybody gets their own private collection of memoized values upon class
+# instantiation.  Don't like it?  Too bad.
+class TripInfo(dict):
+	def __init__(self, *args):
+		self.trip_cost = Mpg.trip_cost_memoizable()
+		self.pergallon = Mpg.pergallon_memoizable()
+		super().__init__(*args)
+
+	@property
+	def cost(self, volume = 0, cost = 0):
+		volume = (self.get('fuel_volume') or 0)
+		cost = (self.get('perunit_cost') or 0)
+		return self.trip_cost(volume, cost)
+	@property
+	def mpg(self, distance = 0, volume = 0):
+		distance = (self.get('distance') or 0)
+		volume = (self.get('fuel_volume') or 0)
+		return self.pergallon(distance, volume)
+
+# Build up a dictionary again
+
+mpg = TripInfo({'linear_unit': input("\n".join(textwrap.wrap("What unit (singular, decapitalized, unless it's a proper noun) do you use to measure distance?:", 72)) + "  ")}) # Holy moses textwrap is irritating.
+mpg['volumetric_unit'] = input("\n".join(textwrap.wrap("What unit (lower-case, unless a proper pronoun) do you use to measure volume?:", 72)) + "  ")
+mpg['distance'] = float(input("\n".join(textwrap.wrap("What was the length of your trip in {linear_unit:s}s?:".format(**mpg), 72)) + "  "))
+mpg['fuel_volume'] = float(input("\n".join(textwrap.wrap("How many {volumetric_unit:s}s did you use on your trip?:".format(**mpg), 72)) + "  "))
+mpg['perunit_cost'] = float(input("\n".join(textwrap.wrap("What was the cost (in currency, unprefixed) per {volumetric_unit:s} of gas?:".format(**mpg), 72)) + "  "))
+
+# Right, so now we get to see the fruits of our labors.
+print("\n".join(textwrap.wrap("OK, here's what we know: Your trip was {distance:f} {linear_unit:s}s long.\nYou expended {fuel_volume:f} {volumetric_unit:s}s of gas.\nAt {perunit_cost:f} per {volumetric_unit:s}, your trip cost approximately {trip_cost:f} units of currency.\nFor the entirety of the trip, you averaged {pergallon:f} miles per gallon.".format(trip_cost = mpg.cost, pergallon = mpg.mpg, **mpg), 72)))
+# ...and we're done.  Phew.
 
