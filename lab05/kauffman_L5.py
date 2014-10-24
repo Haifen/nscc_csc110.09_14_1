0d4e8a3da83d43ecf0d84320cfddbc6b1b9d0be8
Robin Kauffman <robink@creosotehill.org>
2014-10-23 19:48:04 -0700

    Comments and better RE
    Added comments to (hopefully) explain how sorry I am that I went about
    doing things this way, and that I'm hoping there's assumedly a correct way
    to do this in Python, which probably isn't implementing reduce_while().
    Unlikely anyone will see this, but if I'm that angsty about my Moodler
    submission, I'll link Mr. Wu to this.

--- lab05/kauffman_L5.py
+++ lab05/kauffman_L5.py
@@ -9,13 +9,13 @@ import re
 ## MSOI is acryonymized "My Series of Identities"
 msoi = list()
 ## Assume anything the user enters may be cast as an int()
-## Assume anything that's not 0-9 is the user indicating they're done with text entry.
-validentry_re = re.compile("^(\d+)?(.+)?$", re.I)
+## Assume anything that's not [+-]?[0-9]+ is the user indicating they're done with text entry.
+validentry_re = re.compile("^([+-]?\d+)?(.+)?$", re.I)
 
 doneness_canary = False
 
 while(doneness_canary == False):
-  inm = re.match(validentry_re, input("Please enter a positive integer: "))
+  inm = re.match(validentry_re, input("Please enter an integer: "))
   if(inm.group(1)):
     msoi.append(int(inm.group(1)))
   else:
@@ -43,3 +43,8 @@ if(matched):
 else:
   print("Sorry, we couldn't find the sequence [1, 2, 3] in your series.")
 
+
+  # This super-ugly "recursive" descent search needs to be properly implemented as a function that takes a variable-arity search list and always knows to quit when it's ahead.
+  # If I were doing this "functionally", my crude approach would be to try to implement a general-purpose "reduce-while" function that would not only take a closure, a collection and an optional initial value but would also allow whomever was calling it to throw in a predicate (bound function or lambda) that would tell it to give up when that block returned False.
+  # What I ought to implement, given that this is Python, would be a "higher-order" function that returned some object implementing an iterable interface that stepped over its contents only returning the identity of an item in the list when it was a matching component of a series and only set a boolean attribute and a (list of) inde(ces|x) of the match(es) when the entire set was evaluated.  This has the rather glaring flaw of returning not only a value, but a value that evaluates to True when only a subset of a match series has been encountered.  Thus, while it would be kinda cool, the identities in an iterable wouldn't be an accurate representation of the ultimate endgame (the index or indeces of where in the original list the match(es) began).
+  # Lastly, I wouldn't know how to force in the extra object attributes (did_match = bool(), match_indeces = list()) were my little iterable "collection" cast to a proper sequential Python type (list(), etc), so the information that someone making use of this instance would really care about (did we match?  Where did we match?  If we care about partial matches, how well did we match?) would be completely thrown away.
