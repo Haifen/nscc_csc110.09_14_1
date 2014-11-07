cc8d6dd63fe0f261dd37b2ae1851eb59f624d9c2
Robin Kauffman <robink@creosotehill.org>
2014-11-06 17:24:17 -0800

    Kauffman_L6_1.py update w/ self-defined function
    Slight fiddle.  It calculates the areas of triangles, I guess?

--- lab06/Kauffman_L6_1.py
+++ lab06/Kauffman_L6_1.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 
-# $Header: nscc_csc110.09_14_1/lab06/Kauffman_L6_1.py, r3 201411061704 US/Pacific-New PST UTC-0800 robink@northseattle.edu Lab $
+# $Header: nscc_csc110.09_14_1/lab06/Kauffman_L6_1.py, r4 201411061724 US/Pacific-New PST UTC-0800 robink@northseattle.edu Lab $
 # Lab 3 starter code
 
 
@@ -17,17 +17,16 @@ def main():
      print("\n\nFunction inputs and outputs:\n\nBase1\t\tBase2\t\tHeight\t\tArea (computed)\n{1}\t\t{2}\t\t{3}\t\t{0}".format(areaTrapezoid(*ar_tr_arg), *ar_tr_arg))
 
 
-  yesno = re.compile("^.*([Yy])?.*$")
+  yesno = re.compile("^.*([Yy]).*$")
   # put your function calls here
   coord_pair = [3, 5]
 
   if(re.match(yesno, input("\n".join(textwrap.wrap("Would you like to enter a set of base linear dimensions for the trangle, or would you prefer to stick with the internal constants (Y to enter 90º dimensions, N to stick with constants) (Y/N): ", 72)))).group(1)):
-    ud_coord_pair = list(int(input("Please enter the width of the triangle: ")))
+    coord_pair[0] = float(input("Please enter the width of the triangle: "))
+    coord_pair[1] = (float(input("Please enter the height of the triangle: ")))
+  ud_tr_result = areaTriangle(*coord_pair)
+  print("\n".join(textwrap.wrap("For a triangle of width {1} and height {2}, the area is {0}".format(ud_tr_result, *coord_pair))))
 
-
-
-
-  
 # This function calculates and returns the area of a trapezoid
 # parameter: base1, the length of the top of the trapezoid
 # parameter: base2, the length of the bottom
@@ -40,5 +39,7 @@ def areaTrapezoid(base1, base2, height):
 
     return area
 
+def areaTriangle(width, height):
+  return width * height / 2.0
 
 main()
